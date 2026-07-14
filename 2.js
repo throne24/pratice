dd497e6a1a37de1278bcb6222394013a7985eda0
SIR <2972749758@11.com>
2026-07-14 17:43:23 +0800

    箭头函数练习

---
+++ 2.js
@@ -0,0 +1,7 @@
+const getUser = () =>{name:"John"};
+const bar = function arr(){};//具名函数表达式
+const bar1 = function (){};//匿名函数表达式
+
+console.log(bar.name);
+console.log(bar1.name);
+console.log(getUser.name);
